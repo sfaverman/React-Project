0b98a22a5339269496cd33000a49504365a5c1f7
sfaverman <sofiasd@yahoo.com>
2020-05-16 20:04:17 -0700

    step 2 - routing and sngle content views

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header"
 import Posts from "./components/Posts"
+import Post from "./components/Post"
+import NotFound from "./components/NotFound"
+
+//need to setup React Router:
+//    npm install react-router-dom */
+//
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+}    from "react-router-dom";
 
 class App extends Component {
     state = {
       posts: [
           {
             id: 1,
+            slug: "hello-react",
             title: "Hello React",
             content: "Lorem."
           },
           {
             id: 2,
+            slug: "hello-project",
             title: "Hello Project",
             content: "Tothe."
           },
           {
             id: 3,
+            slug: "hello-blog",
             title: "Hello Blog",
             content: "Ipsum."
           },
@@ -25,11 +39,32 @@ class App extends Component {
     };
     render() {
         return (
-        <div className="App">
-            <Header />
-            <Posts posts={this.state.posts} />
-            APP HERE
-        </div>
+           //Application is managed by React Router
+           //call <Posts /> lawhen the main root of your site is accessed
+        <Router>
+            <div className="App">
+                <Header />
+                <Switch>
+                  <Route exact path="/"
+                    render={() =>
+                    <Posts posts={this.state.posts}
+                    />}
+                  />
+                  <Route path="/post/:postSlug"
+                    render={props => {
+                      const post =    this.state.posts.find(
+                         post => post.slug === props.match.params.postSlug
+                        );
+                        if (post)
+                          return <Post post={post} />;
+                        else return <NotFound />;
+                    }}
+                  />
+                 <Route component={NotFound} />
+
+                </Switch>
+            </div>
+        </Router>
         );
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const Header = props => (
     <header className="App-header">
         <ul className="container">
-            <li>Site Title</li>
+            <li key="home">
+                <Link to="/">My Site</Link>
+            </li>
         </ul>
     </header>
 
