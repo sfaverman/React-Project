9c6eac5da60f054bf7c46a0fdaab6bc922fa8d4a
sfaverman <sofiasd@yahoo.com>
2020-05-19 11:12:02 -0700

    Step 5 Updating Post content -  Edit Post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import Header from "./components/Header";
 import Message from "./components/Message";
 import Posts from "./components/Posts";
@@ -7,28 +12,23 @@ import Post from "./components/Post";
 import PostForm from "./components/PostForm";
 import NotFound from "./components/NotFound";
 
-//need to setup React Router:
-//    npm install react-router-dom */
-//
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-}    from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
-    state = {
-      posts: [],
-      message: null
-    };
-  addNewPost = post => {
-    post.id = this.state.posts.length + 1;
-    post.slug = encodeURIComponent(
-      post.title
+  state = {
+    posts: [],
+    message: null
+  };
+  getNewSlugFromTitle = title =>
+    encodeURIComponent(
+      title
         .toLowerCase()
         .split(" ")
         .join("-")
     );
+  addNewPost = post => {
+    post.id = this.state.posts.length + 1;
+    post.slug = this.getNewSlugFromTitle(post.title);
     this.setState({
       posts: [...this.state.posts, post],
       message: "saved"
@@ -37,43 +37,75 @@ class App extends Component {
       this.setState({ message: null });
     }, 1600);
   };
-    render() {
-        return (
-           //Application is managed by React Router
-           //call <Posts /> lawhen the main root of your site is accessed
-        <Router>
-            <div className="App">
-                <Header />
-
-                {this.state.message && <Message type={this.state.message} />}
-
-                <Switch>
-                  <Route exact path="/"
-                    render={() =>
-                    <Posts posts={this.state.posts}
-                    />}
-                  />
-                  <Route path="/post/:postSlug"
-                    render={props => {
-                      const post =    this.state.posts.find(
-                         post => post.slug === props.match.params.postSlug
-                        );
-                        if (post)
-                          return <Post post={post} />;
-                        else return <NotFound />;
-                    }}
-                  />
-                <Route
-                  exact               path="/new"
-                  render={() => <PostForm addNewPost={this.addNewPost} />}
+  updatePost = post => {
+    post.slug = this.getNewSlugFromTitle(post.title);
+    const index = this.state.posts.findIndex(p => p.id === post.id);
+    const posts = this.state.posts
+      .slice(0, index)
+      .concat(this.state.posts.slice(index + 1));
+    const newPosts = [...posts, post].sort((a, b) => a.id - b.id);
+    this.setState({
+      posts: newPosts,
+      message: "updated"
+    });
+    setTimeout(() => {
+      this.setState({ message: null });
+    }, 1600);
+  };
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          {this.state.message && <Message type={this.state.message} />}
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <Posts posts={this.state.posts} />}
+            />
+            <Route
+              path="/post/:postSlug"
+              render={props => {
+                const post = this.state.posts.find(
+                  post => post.slug === props.match.params.postSlug
+                );
+                if (post) {
+                  return <Post post={post} />;
+                } else {
+                  return <Redirect to="/" />;
+                }
+              }}
+            />
+            <Route
+              exact
+              path="/new"
+              render={() => (
+                <PostForm
+                  addNewPost={this.addNewPost}
+                  post={{ id: 0, slug: "", title: "", content: "" }}
                 />
-                 <Route component={NotFound} />
-
-                </Switch>
-            </div>
-        </Router>
-        );
-    }
+              )}
+            />
+            <Route
+              path="/edit/:postSlug"
+              render={props => {
+                const post = this.state.posts.find(
+                  post => post.slug === props.match.params.postSlug
+                );
+                if (post) {
+                  return <PostForm updatePost={this.updatePost} post={post} />;
+                } else {
+                  return <Redirect to="/" />;
+                }
+              }}
+            />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,22 +4,36 @@ import Quill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
 class PostForm extends Component {
-    state = {
-        title: " ",
-        content: " ",
-        saved: false
-      };
-    handlePostForm = e => {
+  state = {
+    post: {
+      id: this.props.post.id,
+      slug: this.props.post.slug,
+      title: this.props.post.title,
+      content: this.props.post.content
+    },
+    saved: false
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.post.id !== this.props.post.id) {
+      this.setState({
+        post: {
+          id: this.props.post.id,
+          slug: this.props.post.slug,
+          title: this.props.post.title,
+          content: this.props.post.content
+        }
+      });
+    }
+  }
+  handlePostForm = e => {
     e.preventDefault();
-    if (this.state.title) {
-      const post = {
-        title: this.state.title,
-        content: this.state.content
-      };
-        console.log(post);
-       this.props.addNewPost(post);
-       this.setState({ saved: true });
-
+    if (this.state.post.title) {
+      if (this.props.updatePost) {
+        this.props.updatePost(this.state.post);
+      } else {
+        this.props.addNewPost(this.state.post);
+      }
+      this.setState({ saved: true });
     } else {
       alert("Title required");
     }
@@ -30,30 +44,43 @@ class PostForm extends Component {
     }
     return (
       <form className="container" onSubmit={this.handlePostForm}>
-        <h1>Add a New Post </h1>
+        <h1>Add a New Post</h1>
         <p>
           <label htmlFor="form-title">Title:</label>
           <br />
-          <input id="form-title"
-          value={this.state.title}
-          onChange={e => this.setState({
-            title: e.target.value
-            })}
-           />
+          <input
+            defaultValue={this.props.title}
+            id="form-title"
+            value={this.state.post.title}
+            onChange={e =>
+              this.setState({
+                post: {
+                  ...this.state.post,
+                  title: e.target.value
+                }
+              })
+            }
+          />
         </p>
         <p>
           <label htmlFor="form-content">Content:</label>
         </p>
         <Quill
+          defaultValue={this.state.post.content}
           onChange={(content, delta, source, editor) => {
-            this.setState({ content: editor.getContents() });    }}
+            this.setState({
+              post: {
+                ...this.state.post,
+                content: editor.getContents()
+              }
+            });
+          }}
         />
-
         <p>
           <button type="submit">Save</button>
         </p>
       </form>
     );
-    }
+  }
 }
 export default PostForm;
